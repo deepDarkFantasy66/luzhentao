b5d44c31329266cebb9fea1c22f32cf964fc9078
jhon <1015512974@qq.com>
2023-09-06 17:30:41 +0800

    测试map转成bean

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+    import java.util.HashMap;
+import java.util.Map;
+
+    public class Main {
+        public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+            Map<String, Object> map = new HashMap<>();
+            map.put("username", "John");
+            map.put("age", 25);
+            map.put("salary", 50000.0);
+
+            User user = BeanMapper.mapToBean(map, User.class);
+            System.out.println(user.getUsername()); // 输出: John
+            System.out.println(user.getAge()); // 输出: 25
+            System.out.println(user.getSalary()); // 输出: 50000.0
+        }
+    }
+
