184283c152d64c796f70c5dbb6b9a7315dbe7cfe
jhon <1015512974@qq.com>
2023-11-09 11:00:19 +0800

    测试map转成bean

--- src/main/java/com/example/demo/JsonParser.java
+++ src/main/java/com/example/demo/JsonParser.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 
+import org.json.JSONException;
+
 public class Test {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws JSONException {
             UserMapper userMapper = new UserMapper();
             ResumeMapper resumeMapper = new ResumeMapper();
             JsonParser parser = new JsonParser(userMapper, resumeMapper);
