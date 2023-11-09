06c259410634967527ca4f306edfdcb89883e3f8
jhon <1015512974@qq.com>
2023-11-09 11:13:39 +0800

    测试map转成bean

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -9,36 +9,7 @@ public class Test {
             ResumeMapper resumeMapper = new ResumeMapper();
             JsonParser parser = new JsonParser(userMapper, resumeMapper);
 
-            String jsonString = "{\n" +
-                    "  \"type\": \"object\",\n" +
-                    "  \"properties\": {\n" +
-                    "    \"name\": {\n" +
-                    "      \"type\": \"John Doe\"\n" +
-                    "    },\n" +
-                    "    \"age\": {\n" +
-                    "      \"type\": 30,\n" +
-                    "      \"minimum\": 1,\n" +
-                    "      \"maximum\": 150\n" +
-                    "    },\n" +
-                    "    \"resume\": {\n" +
-                    "      \"type\": \"array\",\n" +
-                    "      \"items\": {\n" +
-                    "        \"type\": \"object\",\n" +
-                    "        \"properties\": {\n" +
-                    "          \"companyName\": {\n" +
-                    "            \"type\": \"Company A\"\n" +
-                    "          },\n" +
-                    "          \"startTime\": {\n" +
-                    "            \"type\": \"2020-01-01\"\n" +
-                    "          },\n" +
-                    "          \"endTime\": {\n" +
-                    "            \"type\": \"2022-01-01\"\n" +
-                    "          }\n" +
-                    "        }\n" +
-                    "      }\n" +
-                    "    }\n" +
-                    "  }\n" +
-                    "}";
+            String jsonString = "";
 
             parser.parseJson(jsonString);
     }
