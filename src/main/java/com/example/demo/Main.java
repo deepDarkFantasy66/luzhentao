d4a381b6fc8174b91118f867797d7b96ee5496b9
luzhentao <1015512974@qq.com>
2024-01-01 10:48:51 +0800

    1111

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,24 +2,20 @@ package com.example.demo;
 
     import java.util.HashMap;
 import java.util.Map;
+    import java.util.Scanner;
 
-    public class Main {
+public class Main {
         public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-            Map<String, Object> map = new HashMap<>();
-            map.put("username", "John");
-            map.put("age", 25);
-            map.put("salary", 50000.0);
-
-            User user = BeanMapper.mapToBean(map, User.class);
-            System.out.println(user.getUsername()); // 输出: John
-            System.out.println(user.getAge()); // 输出: 25
-            System.out.println(user.getSalary()); // 输出: 50000.0
+            switchTest();
         }
-        public static void main(String str) {
-            str = "welcome";
-            int i = 130;
-            byte b = (byte) i;
-            System.out.printf("str = %s, i = %d, b = %d", str, i, b);
+        public static void switchTest(){
+        System.out.println("请输入");
+        int next = new Scanner(System.in).nextInt();
+        int i = 1;
+            switch (next) {
+                case 1, 2,3,4,5 ->   System.out.println("工作日");
+                case 6,7 ->        System.out.println("休息日");
+                default->         System.out.println("输入错误");
+            }
         }
-
     }
