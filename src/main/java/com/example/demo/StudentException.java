332ef2aae9765e87f4e910ff98df6fe61da33b49
jhon <1015512974@qq.com>
2024-01-18 09:25:14 +0800

    自定义异常处理

---
+++ src/main/java/com/example/demo/StudentException.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class StudentException extends Exception {
+    public StudentException() {
+        super();
+    }
+
+    public StudentException(String message) {
+        super(message);
+    }
+}
