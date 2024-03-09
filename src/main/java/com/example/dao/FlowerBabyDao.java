530bc94f07e487bf3d703bd81c5abd628b6c20ec
jhon <1015512974@qq.com>
2024-03-09 10:35:12 +0800

    定义前端请求路径

--- src/main/java/com/example/dao/FlowerBabyDao.java
+++ src/main/java/com/example/dao/FlowerBabyDao.java
@@ -2,9 +2,12 @@ package com.example.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.bean.FlowerBaby;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface FlowerBabyDao  {
-    void saveFlowerBaby(FlowerBaby flowerBaby);
+public interface FlowerBabyDao extends BaseMapper<FlowerBaby> {
+
+//        @Insert("INSERT INTO demo (name, age) VALUES (#{name}, #{age})")
+        int saveFlowerBaby(FlowerBaby flowerBaby);
 }
